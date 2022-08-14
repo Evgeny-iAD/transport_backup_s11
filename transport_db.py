6be9134d6fe7bb086449e7904d9c40bf8d0db2ea
iAD <iadmail1987@gmail.com>
2022-08-14 18:09:16 +0300

    Корректировака файла транспорт

--- transport_db.py
+++ transport_db.py
@@ -5,9 +5,7 @@ import logging
 import warnings
 import json
 from datetime import date, timedelta, datetime
-
 from PyQt5.QtCore import Qt
-
 from maket.Transpotr_maket import *
 from Date_dum import date_dump
 
@@ -30,14 +28,14 @@ def chtenie_json(filename):
     try:
         with open(filename) as f:
             return f.read()
-    except: print('конфиг не найден')
+    except:
+        print('конфиг не найден')
+
 
 def zapis_json(filename, host, remoteP, localP):
     with open(filename, 'w') as f:
         data_json = {'host': host, 'remoteP': remoteP, 'localP': localP}
         json.dump(data_json, f)
-
-
 
 
 class Path_file:
@@ -71,7 +69,8 @@ class Export_DB:
             print(self.remotepath)
             print(self.localpath)
             print(self.host)
-            with pysftp.Connection(host=self.host, port=22, username=self.user, password=self.secret, cnopts=self.cnopts) as sftp:
+            with pysftp.Connection(host=self.host, port=22, username=self.user, password=self.secret,
+                                   cnopts=self.cnopts) as sftp:
                 try:
                     date_dump = []
                     print(self.remotepath)
@@ -98,52 +97,54 @@ class Export_DB:
             logging.error('Ошибка передачи данных')
         return date_dump
 
-
-
-
     def transport(self):
         # self.progres_start()
-        print('Подключение к серверу...') #12,5%
+        print('Подключение к серверу...')  # 12,5%
         ui.progressBar.setValue(12)
         try:
             with pysftp.Connection(host=self.host, port=22, username=self.user, password=self.secret,
                                    cnopts=self.cnopts, ) as sftp:
-                print('Подключение к серверу прошло успешно...') #25%
+                print('Подключение к серверу прошло успешно...')  # 25%
                 ui.progressBar.setValue(25)
                 ui.progressBar.setFormat('Подключение к серверу прошло успешно...')
                 ui.progressBar.setAlignment(Qt.AlignCenter)
-                print('Начинаю процесс копирования s11...') #37,5%
+                print('Начинаю процесс копирования s11...')  # 37,5%
                 ui.progressBar.setValue(37)
                 ui.progressBar.setFormat('Начинаю процесс копирования s11...')
                 ui.progressBar.setAlignment(Qt.AlignCenter)
                 sftp.get(
-                    Path_file(self.remotepath, 's11', f'{ui.calendarWidget.selectedDate().toString("yyyy-MM-dd")}').__str__(),
-                    Path_file(self.localpath, 's11', f'{ui.calendarWidget.selectedDate().toString("yyyy-MM-dd")}').__str__()
+                    Path_file(self.remotepath, 's11',
+                              f'{ui.calendarWidget.selectedDate().toString("yyyy-MM-dd")}').__str__(),
+                    Path_file(self.localpath, 's11',
+                              f'{ui.calendarWidget.selectedDate().toString("yyyy-MM-dd")}').__str__()
                 )
-                print('Процесс копирования дампа s11 завершен') #50%
+                print('Процесс копирования дампа s11 завершен')  # 50%
                 ui.progressBar.setValue(50)
                 ui.progressBar.setFormat('Процесс копирования дампа s11 завершен')
                 ui.progressBar.setAlignment(Qt.AlignCenter)
             with pysftp.Connection(host=self.host, port=22, username=self.user, password=self.secret,
                                    cnopts=self.cnopts, ) as sftp:
-                print('Подключение к серверу прошло успешно...') #62,5%
+                print('Подключение к серверу прошло успешно...')  # 62,5%
                 ui.progressBar.setValue(62)
                 ui.progressBar.setFormat('Подключение к серверу прошло успешно...')
                 ui.progressBar.setAlignment(Qt.AlignCenter)
-                print('Начинаю процесс копирования mes...') #75%
+                print('Начинаю процесс копирования mes...')  # 75%
                 ui.progressBar.setValue(75)
                 ui.progressBar.setFormat('Начинаю процесс копирования mes...')
                 ui.progressBar.setAlignment(Qt.AlignCenter)
                 sftp.get(
-                    Path_file(self.remotepath, 'mes', f'{ui.calendarWidget.selectedDate().toString("yyyy-MM-dd")}').__str__(),
-                    Path_file(self.localpath, 'mes', f'{ui.calendarWidget.selectedDate().toString("yyyy-MM-dd")}').__str__()
+                    Path_file(self.remotepath, 'mes',
+                              f'{ui.calendarWidget.selectedDate().toString("yyyy-MM-dd")}').__str__(),
+                    Path_file(self.localpath, 'mes',
+                              f'{ui.calendarWidget.selectedDate().toString("yyyy-MM-dd")}').__str__()
                 )
-                print('Процесс копирования дампа mes завершен') #87,5%
+                print('Процесс копирования дампа mes завершен')  # 87,5%
                 ui.progressBar.setValue(87)
                 ui.progressBar.setFormat('Процесс копирования дампа mes завершен')
                 ui.progressBar.setAlignment(Qt.AlignCenter)
-        except: print('Дамп БД не найден!')
-        print('процедура загрузки дампов завершена') #100%
+        except:
+            print('Дамп БД не найден!')
+        print('процедура загрузки дампов завершена')  # 100%
         ui.progressBar.setValue(100)
         ui.progressBar.setFormat('Процедура загрузки дампов завершена')
         ui.progressBar.setAlignment(Qt.AlignCenter)
@@ -162,18 +163,19 @@ class Calendar_dump_v2(QtWidgets.QCalendarWidget):
             # qqdate = QtCore.QDate.fromString(dat, "yyyy-MM-dd")
             # ui.calendarWidget.setSelectedDate(qqdate)
             if date.toString('yyyy-MM-dd') == dat:
-                 painter.setBrush(QtGui.QColor(0, 200, 200, 50))
-        #         # painter.setPen(QtGui.QPen(QtGui.QColor(0, 200, 200),  3, Qt.SolidLine, Qt.RoundCap))
-        #         # painter.setPen(QtGui.QColor(0, 0, 0, 0))
-        #         # painter.drawLine(rect.topRight(), rect.topLeft())
-        #         # painter.drawLine(rect.topRight(), rect.bottomRight())
-        #         # painter.drawLine(rect.bottomLeft(), rect.bottomRight())
-        #         # painter.drawLine(rect.topLeft(), rect.bottomLeft())
-                 painter.drawRect(rect)
+                painter.setBrush(QtGui.QColor(0, 200, 200, 50))
+                #         # painter.setPen(QtGui.QPen(QtGui.QColor(0, 200, 200),  3, Qt.SolidLine, Qt.RoundCap))
+                #         # painter.setPen(QtGui.QColor(0, 0, 0, 0))
+                #         # painter.drawLine(rect.topRight(), rect.topLeft())
+                #         # painter.drawLine(rect.topRight(), rect.bottomRight())
+                #         # painter.drawLine(rect.bottomLeft(), rect.bottomRight())
+                #         # painter.drawLine(rect.topLeft(), rect.bottomLeft())
+                painter.drawRect(rect)
 
 
 if __name__ == "__main__":
     import sys
+
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
@@ -184,6 +186,7 @@ if __name__ == "__main__":
     ui.lineEdit.setText(obj['host'])
     ui.lineEdit_2.setText(obj['remoteP'])
     ui.lineEdit_3.setText(obj['localP'])
+
 
     def on_click_scan():
         remotepath = ui.lineEdit_2.text()
@@ -197,31 +200,13 @@ if __name__ == "__main__":
             f.write(f'date_dump = {str(x)}')
         zapis_json('connect.json', host_db, remotepath, localpath)
 
+
     def on_click_transport():
         expo = Export_DB(ui.lineEdit.text(), user_db, secret_db, cnopts, ui.lineEdit_2.text(), ui.lineEdit_3.text())
         expo.transport()
+
 
     ui.pushButton.clicked.connect(on_click_scan)
     ui.pushButton_2.clicked.connect(on_click_transport)
 
     sys.exit(app.exec_())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
